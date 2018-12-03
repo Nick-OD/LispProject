c4ecd5dca9cfc1e30ee05f70efc945f8d52bd4fe
Nick O'Donnell <odonnell.nick28@gmail.com>
2018-12-02 20:09:03 -0500

    What is ofasl

--- LispProject/Test.lisp
+++ LispProject/Test.lisp
@@ -1,10 +1,5 @@
 ;;;; -*- Mode: Lisp; -*-
 
-
-(setq famTree (make-hash-table :test #'string= ))
-
-
-
 (defstruct person 
   (name nil)
   (parents (list))
@@ -16,13 +11,21 @@
 
 (defun storeperson (name struct tree)
 ;  "Students need to write this! This should enter the person structure in STRUCT into the hashtable in FAMY-TREE with the key in SYMBOL-NAME."
-(setf (gethash name tree) person))
+(setf (gethash name tree) struct))
 
 
 (defun personstruct (name tree)
  ; "Returns a structure of type person corresponding to the key SYMBOL-NAME in the hashtable FAMILY-TREE. If there is no one in the tree with the name in SYMBOL-NAME, returns NIL."
   (gethash name tree nil))
 
+(defun addspouse (spouse-name p)
+  (setf (person-spouses p) (cons spouse-name (person-spouses p)))
+;  (setf (person-spouses p) (sort (person-spouses p) #'string<
+)
+
+(defun addchild (child-name p)
+  (setf (person-children p) (cons child-name (person-children p)))
+)
 
 (defun getparents (p)
   (person-parents (gethash p famTree)))
@@ -34,11 +37,42 @@
   (person-spouses (gethash p famTree)))
 
 (defun ancestors (p tree)
-  (let ((parent1 (get-person (first (person-parents p))tree ))
-        (parent2 (get-person (second (person-parents p))tree )))
+  (let ((parent1 (personstruct(first (person-parents p))tree ))
+        (parent2 (personstruct (second (person-parents p))tree )))
     (if (null parent1)
          nil
-       (append (list parent1 parent2) (ancestors parent1 tree )(ancestors parent2 tree )))))
+       (append (list(person-name parent1) (person-name parent2)) (ancestors parent1 tree )(ancestors parent2 tree )))))
+
+; is functions
+(defun isspouse (p1 p2)
+; Return boolean true or false
+(let ((spouse nil))
+(if  (member (person-name p1) (person-spouses p2) :test #'equal) (setf spouse t))
+(if spouse t nil))) ; If spouse, spouse is true, else spouse is false 
+
+)
+
+(defun ischild (p1 p2)
+
+)
+
+(defun issibling (p1 p2 tree)
+
+)
+
+(defun iscousin (p1 p2 tree)
+
+)
+
+(defun isancestor (p1 p2 tree)
+
+)
+
+(defun isunrelated (p1 p2 tree)
+
+)
+
+
 
 
 
@@ -46,36 +80,41 @@
 
 
 (defun family()
+  (let ((tree (make-hash-table :size 1000 :test #'equal)))
  (let ((line nil) (tokens nil) (p1 nil) (p2 nil) (p3 nil))
-       (setf queries (list 'E 'X 'W))
-       (setf line (list (E John Mary Zach)))
-;  (with-open-file (str Testcase.txt
-;                     :direction INPUT
-;                     :if-does-not-exist )
-;    (do ((line (read-line stream nil)
-;             (read-line stream nil)))
-;        ((null line))
-      (print line)
-     ; (let tokens (split-sequence " " line))
-      (cond ((equal (first line) (first queries))
-             (;Person creation and spouse children assignment here
-              (format t ~a~% line)
-              ; Test to see if tree
-            ;  (setf p1 (gethash (second line) famTree))
-           ;   (setf p1 (make-person :name (second line)))
-           ;   (setf storeperson(p1 person famTree))
+(let (line (read *standard-input*))
+   (loop for line
+         until (eq line nil)
+         do (
+             print line
+      (cond ((string= (first line) "E" )
+             ;Person creation and spouse children assignment here
+              (p1 (second line))
+              (p2 (third line))
+              
+              ; Test to see if tree, store if not
+              (if (= (personstruct p1 tree) nil)
+                (setf person1 (storeperson p1 (make-person :name p1) tree))
+                )
+              (if (= (personstruct p2 tree) nil)
+                  (setf person2 (storeperson p2 (make-person :name p2) tree))
+                )
+              
+              (loop for key being each hash-key of tree
+                    using (hash-value value)
+                    collect (list key value))
+              (format t "~%" tree)
 
-            ;  (setf p2 (gethash (second line) famTree))
-            ;  (setf p2 (make-person :name (second line)))
-           ;   (setf storeperson(p2 person famtree))
-              ))
-            ((equal (first line) (second queries))
-             (;Xquery stuff here
+            )
+            ((string= (first line) "X")
+             ;Xquery stuff here
+              (print (second line))
+
+
+              )
+            ((string= (first line) "W")
+            ;Wquery stuff here
               (print (second line))
               ))
-            ((equal (first line) (second queries))
-            (;Wquery stuff here
-              (print (second line))
-             ))
 
-)))
+)))))))
